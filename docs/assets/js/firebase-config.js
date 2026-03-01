acdd8315f17bea792792e89265ad7f08fa843831
Saurabh Maurya <saurabhmaurya06@gmail.com>
2026-03-01 15:12:22 -0800

    Docs: Live Firebase configuration

--- docs/assets/js/firebase-config.js
+++ docs/assets/js/firebase-config.js
@@ -5,12 +5,13 @@
 
 // TODO: Replace this with your actual Firebase config from the console!
 const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-    projectId: "YOUR_PROJECT_ID",
-    storageBucket: "YOUR_PROJECT_ID.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
+    apiKey: "AIzaSyCsaiPL1biWDpHWyLnGX60yLh9LtHBlcC4",
+    authDomain: "swalpa-storage.firebaseapp.com",
+    projectId: "swalpa-storage",
+    storageBucket: "swalpa-storage.firebasestorage.app",
+    messagingSenderId: "683261630195",
+    appId: "1:683261630195:web:8cb8edd93bc922abbd4986",
+    measurementId: "G-E0B6F7LT0P"
 };
 
 if (firebaseConfig.apiKey === "YOUR_API_KEY") {
@@ -23,3 +24,8 @@ firebase.initializeApp(firebaseConfig);
 // Expose instances globally
 window.auth = firebase.auth();
 window.db = firebase.firestore();
+
+// Initialize Analytics if supported
+if (typeof firebase.analytics === 'function') {
+    window.analytics = firebase.analytics();
+}
